64843626eef9f745abd824fd4678897ed9b1a18d
lamatheus.g <m.goncalves@trustetc.com>
2021-11-09 21:51:35 -0300

    make profile id auto incrementable

--- src/main/java/com/conpresp/conprespapi/entity/Profile.java
+++ src/main/java/com/conpresp/conprespapi/entity/Profile.java
@@ -5,15 +5,16 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.UUID;
 
 @Getter @Setter
 @Entity
 public class Profile implements GrantedAuthority {
 
-    @Id
-    private String id = UUID.randomUUID().toString();
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
 
     @Override
