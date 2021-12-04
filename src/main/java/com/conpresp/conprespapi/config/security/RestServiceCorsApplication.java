b862f958a1fdbf2c61b2ad82f3ca75d8c0ca1c2b
lamatheus.g <m.goncalves@trustetc.com>
2021-12-04 11:16:34 -0300

    fix CORS issue

---
+++ src/main/java/com/conpresp/conprespapi/config/security/RestServiceCorsApplication.java
@@ -0,0 +1,27 @@
+package com.conpresp.conprespapi.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class RestServiceCorsApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
+                        .maxAge(-1)
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
